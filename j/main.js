3f02e7965c0a54cd4947c9f3002def8abd9e58e4
WillV56 <will.vetter56@gmail.com>
2023-03-24 23:23:25 -0700

    new3

---
+++ j/main.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", init)
+var myCues;
+function init() {
+    
+    myCues = [
+        { seconds: 2, callback: func1 },
+        { seconds: 7, callback: func2 },
+        { seconds: 22, callback: func3 },
+        { seconds: 27, callback: func4 },
+        { seconds: 31, callback: func5 }
+    ];
+
+    cueTimer.setup("vid", myCues);
+
+    const vid = document.querySelector("#vid");
+
+
+  
+}
+
+function playVideo(clip)
+{
+    clip.play();
+}
+
+function pauseVideo(clip)
+{
+    clip.pause();
+}
+
+function muteVideo(clip)
+{
+    clip.muted = true;
+}
+
+function unmuteVideo(clip) 
+{
+    clip.muted = false;
+}
+
+function playRate(clip, rate)
+{
+    clip.playbackRate = rate;
+}
+
+function selectTrack(e, clip, id)
+{
+    if(clip.textTracks.length > 0) {
+        for (let track of clip.textTracks) {
+            track.mode = 'hidden';
+            track.selected = false;
+        }
+    }
+
+    const theTrack = clip.textTracks.getTrackById(id);
+    console.log(theTrack);
+    theTrack.selected = true;
+    theTrack.mode = 'showing'
+}
+
+function func1() {
+    document.querySelector("#vid").style = "outline : 10px solid purple";
+}
+
+function func2() {
+    
+}
